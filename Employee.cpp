59207af78315f4ec8e71f37ccf78c00b176416e7
Krys <21113012+Krystade@users.noreply.github.com>
2020-02-13 18:51:21 -0800

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -7,6 +7,7 @@
 
 #include "Employee.h"
 
+//Default Employee constructor
 Employee::Employee(){
 	name = "";
 	id = 0;
@@ -15,8 +16,9 @@ Employee::Employee(){
 	gender = 'M';
 	title = "";
 	salary = 0;
-	hireDate = new Date();
+	hireDate = new Date(0, 0, 0);
 }
+//Overloaded Employee constructor
 Employee::Employee(string name, int id, string phone, int age, char gender, string title, int salary, Date* hireDate){
 	this->name = name;
 	this->id = id;
@@ -28,6 +30,7 @@ Employee::Employee(string name, int id, string phone, int age, char gender, stri
 	this->hireDate = hireDate;
 }
 
+//Fuction that prints out an Employee object's data using cout
 void Employee::print(){
 	cout << "Name: " << name << "\nID: " << id << "\nPhone: " << phone << "\nAge: " << age;
 	cout << "\nGender: " << gender << "\nJob Title: " << title << "\nSalary: $" << salary;
