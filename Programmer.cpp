289a9a13c9c096601babcebb76b70277f0555394
Krys <21113012+Krystade@users.noreply.github.com>
2020-02-13 18:51:47 -0800

    Update Programmer.cpp

--- Programmer.cpp
+++ Programmer.cpp
@@ -4,9 +4,9 @@
  *  Created on: Jan 28, 2020
  *      Author: jschmid20
  */
-
 #include "Programmer.h"
 
+//Default Programmer constructor
 Programmer::Programmer():Employee(){
 	departmentNum = 0;
 	supervisorName = " ";
@@ -14,6 +14,7 @@ Programmer::Programmer():Employee(){
 	cpp = true;
 	java = true;
 }
+//Overloaded Programmer constructor
 Programmer::Programmer(string name, int id, string phone, int age, char gender, string title, int salary, Date* hireDate, int departmentNum, string supervisorName, double percentSalaryIncrease, bool cpp, bool java):Employee(name, id, phone, age, gender, title, salary, hireDate){
 	this->departmentNum = departmentNum;
 	this->supervisorName = supervisorName;
@@ -22,6 +23,7 @@ Programmer::Programmer(string name, int id, string phone, int age, char gender, 
 	this->java = java;
 }
 
+//Function that prints out a Programmer object's data using cout
 void Programmer::print(){
 	Employee(*this).print();
 	cout << "Department Number: " << departmentNum << "\nSupervisor's Name: " << supervisorName
